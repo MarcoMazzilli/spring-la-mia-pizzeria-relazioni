00210e9cdd77f516aac4bf10e804c49fafcd4eca
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-10-11 17:24:46 +0200

    [Add] SpecialOffer repository/service

---
+++ src/main/java/org/java/db/repo/SpecialOfferRepo.java
@@ -0,0 +1,8 @@
+package org.java.db.repo;
+
+import org.java.db.pojo.SpecialOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecialOfferRepo extends JpaRepository<SpecialOffer, Integer> {
+	
+}
